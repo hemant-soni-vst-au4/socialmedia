600900755e87d5d96e10d5a22c7d8b1e7eef4115
Hemant soni <sonihemant4741@gmail.com>
2022-04-11 18:13:51 +0530

    Post component add

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import './App.css';
+import { Container, AppBar, Typography, Grow, Grid, Toolbar } from '@material-ui/core';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import useStyles from './style'
+
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      App
-    </div>
+    <Container maxidth="lg">
+      <AppBar className={classes.appBar} position="fixed" color="primary">
+        <Toolbar>
+          <Typography className={classes.heading} variant="h4" align="center">
+            Memories
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts/>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+          
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Post from "./Post/Post";
+
+function Posts() {
+  return (
+    <>
+      <Post />
+      <Post />
+    </>
+  );
+}
+
+export default Posts;
